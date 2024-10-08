a228e15495883fa1cc7f0641e972b27db532f28e
saikumar <saikumar@digisprint.com>
2024-10-08 15:33:58 +0530

    husky config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import './App.css';
+import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-pink-600">Hello Thinkers!</h1>
+      <h1 className="text-3xl font-bold underline text-pink-600">
+        Hello Thinkers!
+      </h1>
     </>
   );
 }
