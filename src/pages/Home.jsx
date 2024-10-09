a044fbabe9900a64379bcdc3642b1bb8ce6988cd
ganesh-digi <msganesh@digisprint.com>
2024-10-09 12:34:46 +0530

    primary secondary update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import Banner from "../components/home/Banner";
+
 const Home = () => {
-  return <div className="text-white">Home</div>;
+  return (
+    <div className="text-white">
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,20 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["'Cambria Math'", "sans-serif"],
+      colors: {
+        primary: {
+          DEFAULT: "#3c5ba4", // Light mode primary color
+          dark: "#e22a90", // Dark mode primary color
+        },
+        secondary: {
+          DEFAULT: "#FEFDFC", // Light mode secondary color
+          dark: "#13111E", // Dark mode secondary color
+        },
+        fontFamily: {
+          sans: ["'Cambria Math'", "sans-serif"],
+        },
       },
     },
+    plugins: [],
   },
-  plugins: [],
 };
