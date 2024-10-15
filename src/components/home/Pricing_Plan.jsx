11a619fc1f1ea8ab339103e53e523d68cff54f7c
ganesh-digi <msganesh@digisprint.com>
2024-10-15 17:20:41 +0530

    feat(comp) features animation section code push

---
+++ src/components/home/Pricing_Plan.jsx
@@ -0,0 +1,9 @@
+function Pricing_Plan() {
+  return (
+    <div>
+      <h1>pricing plan</h1>
+    </div>
+  );
+}
+
+export default Pricing_Plan;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import Banner from "../components/home/Banner";
+import Features_Anime_One from "../components/home/Features_Anime_One";
+import Features_Anime_Three from "../components/home/Features_Anime_Three";
+import Features_Anime_Two from "../components/home/Features_Anime_Two";
 import Features_First from "../components/home/Features_First";
+import Pricing_Plan from "../components/home/Pricing_Plan";
 import "../pages/MainHome.css";
 
 const Home = () => {
@@ -7,6 +11,10 @@ const Home = () => {
     <div className="w-full min-h-screen dark:bg-secondary" id="banner-main">
       <Banner />
       <Features_First />
+      <Features_Anime_One />
+      <Features_Anime_Two />
+      <Features_Anime_Three />
+      <Pricing_Plan />
     </div>
   );
 };
