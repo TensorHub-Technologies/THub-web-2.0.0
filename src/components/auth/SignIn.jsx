a4a9b07ee1f4dbb64d904323cf0e29e76411c196
asamuel-digi <83590864+asamuel-digi@users.noreply.github.com>
2024-11-02 11:25:23 +0530

    Update SignIn.jsx

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -33,7 +33,7 @@ const SignIn = () => {
         localStorage.setItem("token", token);
         alert("user login successful");
 
-        const finalWorkspace = workspace === null ? "app" : workspace;
+        const finalWorkspace = workspace === "undefined" ? "app" : workspace;
         console.log("finalWorkspace: ", finalWorkspace);
         console.log("data?.workspace: ", workspace);
         switch (window.location.hostname) {
