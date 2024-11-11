f1bf4e96007a66ff62c0a4816e2bc298e0fa2cf6
ganesh-digi <msganesh@digisprint.com>
2024-11-11 15:49:41 +0530

    logout and loader code push

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -9,9 +9,12 @@ import { GoMail } from "react-icons/go";
 import { useState } from "react";
 import { signInValidationSchema } from "../../schemas/signInValidationSchema";
 import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword((prev) => !prev);
@@ -31,10 +34,10 @@ const SignIn = () => {
         password,
       });
       if (response.status === 200) {
+        setLoading(true);
         console.log("User inserted successfully");
         const { token, userId, workspace } = response.data;
         localStorage.setItem("token", token);
-        alert("User login successful");
 
         const finalWorkspace = workspace || "app";
         const theme =
@@ -56,103 +59,115 @@ const SignIn = () => {
       }
     } catch (error) {
       console.error("Login failed:", error.response?.data || error.message);
-      alert("Invalid email or password. Please try again.");
+      toast.error(`Invalid email or password.`, {
+        theme: "colored",
+        style: {
+          background: "red",
+          color: "white",
+        },
+      });
     }
   };
 
   return (
-    <Formik
-      initialValues={{
-        email: "",
-        password: "",
-      }}
-      validationSchema={signInValidationSchema}
-      onSubmit={handleSubmit}
-    >
-      {() => (
-        <div className="flex justify-center">
-          <Form className="">
-            <div className="space-y-10">
-              <div className="">
-                <div className="relative">
-                  <span className="absolute left-4 top-[18px] dark:text-secondary-dark ">
-                    <GoMail />
-                  </span>
-
-                  <Field
-                    type="email"
-                    name="email"
-                    placeholder="Enter Your Email"
-                    className="block w-80 pl-12 pr-2 py-2 dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
-                  />
-                </div>
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="text-red-500 text-sm mt-1 absolute"
-                />
-              </div>
-              <div className="mb-4 mt-7">
-                <div className="relative">
-                  <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
-                    <AiOutlineUser />
-                  </span>
-                  <Field
-                    type={showPassword ? "password" : "text"}
-                    name="password"
-                    placeholder="Password"
-                    className="block w-80 pl-12 pr-2 py-2 dark:text-background dark:bg-secondary border shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary dark:focus:ring-primary-dark rounded-md text-lg focus:ring-1"
-                  />
-                  <span
-                    onClick={togglePasswordVisibility}
-                    className="absolute right-4 top-5 cursor-pointer dark:text-secondary-dark"
-                  >
-                    {showPassword ? (
-                      <AiOutlineEyeInvisible />
-                    ) : (
-                      <AiOutlineEye />
-                    )}
-                  </span>
-                </div>
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  className="text-red-500 text-sm mt-1 absolute"
-                />
-              </div>
-            </div>
-
-            <div className="text-end mt-1">
-              <Link
-                to="/auth/forgot-password"
-                className="text-primary dark:text-primary-dark"
-              >
-                Forgot Password?
-              </Link>
-            </div>
-
-            <button
-              type="submit"
-              className="w-full mt-8 py-3 px-6 bg-primary dark:bg-primary-dark text-white dark:text-secondary rounded-lg hover:bg-[#31519b] dark:hover:bg-[#e65ca8]"
-            >
-              Sign In With THub
-            </button>
-
-            <div className="mt-4 text-center">
-              <span className="text-gray-600 dark:text-secondary-dark -ml-6">
-                Don&apos;t have an account?{" "}
-              </span>
-              <Link
-                to="/auth/register"
-                className="text-primary dark:text-primary-dark "
-              >
-                Sign Up for Free
-              </Link>
-            </div>
-          </Form>
-        </div>
+    <div>
+      <ToastContainer />
+      {loading && (
+        <div className="fixed top-0 left-0 w-full h-1 bg-primary dark:bg-primary-dark animate-pulse z-50" />
       )}
-    </Formik>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={signInValidationSchema}
+        onSubmit={handleSubmit}
+      >
+        {() => (
+          <div className="flex justify-center">
+            <Form className="">
+              <div className="space-y-10">
+                <div className="">
+                  <div className="relative">
+                    <span className="absolute left-4 top-[18px] dark:text-secondary-dark ">
+                      <GoMail />
+                    </span>
+
+                    <Field
+                      type="email"
+                      name="email"
+                      placeholder="Enter Your Email"
+                      className="block w-80 pl-12 pr-2 py-2 dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
+                    />
+                  </div>
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-red-500 text-sm mt-1 absolute"
+                  />
+                </div>
+                <div className="mb-4 mt-7">
+                  <div className="relative">
+                    <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
+                      <AiOutlineUser />
+                    </span>
+                    <Field
+                      type={showPassword ? "password" : "text"}
+                      name="password"
+                      placeholder="Password"
+                      className="block w-80 pl-12 pr-2 py-2 dark:text-background dark:bg-secondary border shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary dark:focus:ring-primary-dark rounded-md text-lg focus:ring-1"
+                    />
+                    <span
+                      onClick={togglePasswordVisibility}
+                      className="absolute right-4 top-5 cursor-pointer dark:text-secondary-dark"
+                    >
+                      {showPassword ? (
+                        <AiOutlineEyeInvisible />
+                      ) : (
+                        <AiOutlineEye />
+                      )}
+                    </span>
+                  </div>
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="text-red-500 text-sm mt-1 absolute"
+                  />
+                </div>
+              </div>
+
+              <div className="text-end mt-1">
+                <Link
+                  to="/auth/forgot-password"
+                  className="text-primary dark:text-primary-dark"
+                >
+                  Forgot Password?
+                </Link>
+              </div>
+
+              <button
+                type="submit"
+                className="w-full mt-8 py-3 px-6 bg-primary dark:bg-primary-dark text-white dark:text-secondary rounded-lg hover:bg-[#31519b] dark:hover:bg-[#e65ca8]"
+              >
+                Sign In With THub
+              </button>
+
+              <div className="mt-4 text-center">
+                <span className="text-gray-600 dark:text-secondary-dark -ml-6">
+                  Don&apos;t have an account?{" "}
+                </span>
+                <Link
+                  to="/auth/register"
+                  className="text-primary dark:text-primary-dark "
+                >
+                  Sign Up for Free
+                </Link>
+              </div>
+            </Form>
+          </div>
+        )}
+      </Formik>
+    </div>
   );
 };
 
