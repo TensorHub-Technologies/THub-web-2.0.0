c2c1c013ab5cc6af12ddcb83e9f40ce14f3278fc
asamuel-digi <83590864+asamuel-digi@users.noreply.github.com>
2024-11-11 17:09:27 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,6 @@ import UsecaseHome from "../components/usecases/UsecasesHome";
 
 const Home = () => {
   console.log("*** env: ", import.meta.env.VITE_URL);
-  console.log("*** env gbl: ", $GCP_VITE_URL);
   return (
     <div className="w-full min-h-screen dark:bg-secondary" id="banner-main">
       <Banner />
