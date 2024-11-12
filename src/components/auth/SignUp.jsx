85208cd950211e5834930fd909a9bb336d5a4662
ganesh-digi <msganesh@digisprint.com>
2024-11-12 15:40:35 +0530

    server url update

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -37,7 +37,7 @@ const SignUp = () => {
   const apiUrl =
     window.location.hostname === "localhost"
       ? "http://localhost:2000"
-      : "https://thub-web-ser-2-0ls-dot-thub-dev-420204.uc.r.appspot.com";
+      : "https://thub-web-server-2-0-378678297066.us-central1.run.app";
 
   const checkEmail = async (email) => {
     console.log(apiUrl);
