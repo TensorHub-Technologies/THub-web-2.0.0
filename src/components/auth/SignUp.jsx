8f44bd9434b38ad0c6e6d0582022ead2fe151a44
srishti-digi <smishra@digisprint.com>
2024-11-12 15:42:08 +0530

    ui issue fix

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -198,175 +198,177 @@ const SignUp = () => {
         onSubmit={handleSubmit}
       >
         {() => (
-          <Form className="">
-            <ToastContainer />
-            <div className="space-y-8">
-              {showModal && (
-                <OTP_Modal
-                  length={6}
-                  onOtpSubmit={onOtpSubmit}
-                  setShowModal={setShowModal}
-                  resendOtp={resendOtp}
-                />
-              )}
-              <div className="">
-                <div className="relative">
-                  <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
-                    <AiOutlineUser />
-                  </span>
-                  <Field
-                    type="text"
-                    name="firstName"
-                    placeholder="First Name"
-                    className="block w-full pl-12 pr-2 py-3  dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
+          <div className="flex justify-center">
+            <Form className="">
+              <ToastContainer />
+              <div className="space-y-8">
+                {showModal && (
+                  <OTP_Modal
+                    length={6}
+                    onOtpSubmit={onOtpSubmit}
+                    setShowModal={setShowModal}
+                    resendOtp={resendOtp}
                   />
-                </div>
-                <ErrorMessage
-                  name="firstName"
-                  component="div"
-                  className="text-red-500 text-sm mt-1 absolute"
-                />
-              </div>
-
-              <div className="">
-                <div className="relative">
-                  <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
-                    <AiOutlineUser />
-                  </span>
-                  <Field
-                    type="text"
-                    name="lastName"
-                    placeholder="Last Name"
-                    className="block w-full pl-12 pr-2 py-3  dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
-                  />
-                </div>
-                <ErrorMessage
-                  name="lastName"
-                  component="div"
-                  className="text-red-500 text-sm mt-1 absolute"
-                />
-              </div>
-
-              <div className="">
-                <div className="relative">
-                  <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
-                    <GoMail />
-                  </span>
-                  <Field
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    className="block w-full pl-12 pr-2 py-3  dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
-                  />
-                </div>
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="text-red-500 text-sm mt-1 absolute"
-                />
-              </div>
-
-              <div className="">
-                {!otpSent && (
-                  <div>
-                    <div className="relative">
-                      <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
-                        <BsPhone />
-                      </span>
-                      <Field
-                        type="tel"
-                        name="phoneNumber"
-                        placeholder="Phone Number"
-                        className="block w-full pl-12 pr-2 py-3  dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
-                      />
-                    </div>
-                    <ErrorMessage
-                      name="phoneNumber"
-                      component="div"
-                      className="text-red-500 text-sm mt-1 absolute"
+                )}
+                <div className="">
+                  <div className="relative">
+                    <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
+                      <AiOutlineUser />
+                    </span>
+                    <Field
+                      type="text"
+                      name="firstName"
+                      placeholder="First Name"
+                      className="block w-80 pl-12 pr-2 py-2  dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
                     />
                   </div>
-                )}
-              </div>
+                  <ErrorMessage
+                    name="firstName"
+                    component="div"
+                    className="text-red-500 text-sm mt-1 absolute"
+                  />
+                </div>
 
-              <div className="">
-                <div className="relative">
-                  <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
-                    <CiLock />
-                  </span>
-                  <Field
-                    type={showPassword ? "password" : "text"}
+                <div className="">
+                  <div className="relative">
+                    <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
+                      <AiOutlineUser />
+                    </span>
+                    <Field
+                      type="text"
+                      name="lastName"
+                      placeholder="Last Name"
+                      className="block w-80 pl-12 pr-2 py-2 dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
+                    />
+                  </div>
+                  <ErrorMessage
+                    name="lastName"
+                    component="div"
+                    className="text-red-500 text-sm mt-1 absolute"
+                  />
+                </div>
+
+                <div className="">
+                  <div className="relative">
+                    <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
+                      <GoMail />
+                    </span>
+                    <Field
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      className="block w-80 pl-12 pr-2 py-2  dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
+                    />
+                  </div>
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-red-500 text-sm mt-1 absolute"
+                  />
+                </div>
+
+                <div className="">
+                  {!otpSent && (
+                    <div>
+                      <div className="relative">
+                        <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
+                          <BsPhone />
+                        </span>
+                        <Field
+                          type="tel"
+                          name="phoneNumber"
+                          placeholder="Phone Number"
+                          className="block w-80 pl-12 pr-2 py-2 dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
+                        />
+                      </div>
+                      <ErrorMessage
+                        name="phoneNumber"
+                        component="div"
+                        className="text-red-500 text-sm mt-1 absolute"
+                      />
+                    </div>
+                  )}
+                </div>
+
+                <div className="">
+                  <div className="relative">
+                    <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
+                      <CiLock />
+                    </span>
+                    <Field
+                      type={showPassword ? "password" : "text"}
+                      name="password"
+                      placeholder="Password"
+                      className="block w-80 pl-12 pr-2 py-2  dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
+                    />
+                    <span
+                      onClick={togglePasswordVisibility}
+                      className="absolute right-4 top-5 cursor-pointer dark:text-secondary-dark"
+                    >
+                      {showPassword ? (
+                        <AiOutlineEyeInvisible />
+                      ) : (
+                        <AiOutlineEye />
+                      )}
+                    </span>
+                  </div>
+                  <ErrorMessage
                     name="password"
-                    placeholder="Password"
-                    className="block w-full pl-12 pr-2 py-3  dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
+                    component="div"
+                    className="text-red-500 text-sm mt-1 absolute"
                   />
-                  <span
-                    onClick={togglePasswordVisibility}
-                    className="absolute right-4 top-5 cursor-pointer dark:text-secondary-dark"
-                  >
-                    {showPassword ? (
-                      <AiOutlineEyeInvisible />
-                    ) : (
-                      <AiOutlineEye />
-                    )}
-                  </span>
                 </div>
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  className="text-red-500 text-sm mt-1 absolute"
-                />
-              </div>
 
-              <div className="">
-                <div className="relative">
-                  <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
-                    <CiLock />
-                  </span>
-                  <Field
-                    type={showConfirmPassword ? "password" : "text"}
+                <div className="">
+                  <div className="relative">
+                    <span className="absolute left-4 top-[18px] dark:text-secondary-dark">
+                      <CiLock />
+                    </span>
+                    <Field
+                      type={showConfirmPassword ? "password" : "text"}
+                      name="confirmPassword"
+                      placeholder="Confirm Password"
+                      className="block w-80 pl-12 pr-2 py-2  dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
+                    />
+                    <span
+                      onClick={toggleConfirmPasswordVisibility}
+                      className="absolute right-4 top-5 cursor-pointer dark:text-secondary-dark"
+                    >
+                      {showConfirmPassword ? (
+                        <AiOutlineEyeInvisible />
+                      ) : (
+                        <AiOutlineEye />
+                      )}
+                    </span>
+                  </div>
+                  <ErrorMessage
                     name="confirmPassword"
-                    placeholder="Confirm Password"
-                    className="block w-full pl-12 pr-2 py-3  dark:text-background dark:bg-secondary border  shadow-md border-secondary-dark placeholder-secondary-dark focus:outline-none dark:focus:border-primary-dark focus:border-primary focus:ring-primary  dark:focus:ring-primary-dark  rounded-md text-lg focus:ring-1"
+                    component="div"
+                    className="text-red-500 text-sm mt-1 absolute"
                   />
-                  <span
-                    onClick={toggleConfirmPasswordVisibility}
-                    className="absolute right-4 top-5 cursor-pointer dark:text-secondary-dark"
-                  >
-                    {showConfirmPassword ? (
-                      <AiOutlineEyeInvisible />
-                    ) : (
-                      <AiOutlineEye />
-                    )}
-                  </span>
                 </div>
-                <ErrorMessage
-                  name="confirmPassword"
-                  component="div"
-                  className="text-red-500 text-sm mt-1 absolute"
-                />
+
+                <button
+                  type="submit"
+                  className="block w-80 pl-12 pr-2 py-2 bg-primary dark:bg-primary-dark text-white dark:text-secondary rounded-lg hover:bg-[#31519b] dark:hover:bg-[#e65ca8]"
+                >
+                  {loading ? "Sending OTP..." : "Sign Up"}
+                </button>
               </div>
 
-              <button
-                type="submit"
-                className="w-full py-3 px-6 bg-primary dark:bg-primary-dark text-white dark:text-secondary rounded-lg hover:bg-[#31519b] dark:hover:bg-[#e65ca8]"
-              >
-                {loading ? "Sending OTP..." : "Sign Up"}
-              </button>
-            </div>
-
-            <div className="text-center mt-2">
-              <span className="text-gray-600 dark:text-secondary-dark -ml-6 mr-2">
-                Already have an account?
-              </span>
-              <Link
-                to="/auth/login"
-                className="text-primary dark:text-primary-dark mr-1 "
-              >
-                Sign In
-              </Link>
-            </div>
-          </Form>
+              <div className="text-center mt-2">
+                <span className="text-gray-600 dark:text-secondary-dark -ml-6 mr-2">
+                  Already have an account?
+                </span>
+                <Link
+                  to="/auth/login"
+                  className="text-primary dark:text-primary-dark mr-1 "
+                >
+                  Sign In
+                </Link>
+              </div>
+            </Form>
+          </div>
         )}
       </Formik>
     </div>
