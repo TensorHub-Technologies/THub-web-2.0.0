eaaf09e3c1f79de801bd720b57d79d355b318940
ganesh-digi <msganesh@digisprint.com>
2025-01-13 16:00:11 +0530

    infinite scroll code push

--- src/components/home/LogoSlider.jsx
+++ src/components/home/LogoSlider.jsx
@@ -124,7 +124,7 @@ function LogoSlider() {
       {/* second section */}
       <section className={`py-12 ${isDarkMode ? "#11121C" : "#fffff"}`}>
         <div className="relative overflow-hidden before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-32 before:bg-gradient-to-r dark:before:from-gray-900 after:absolute after:right-0 after:top-0 after:z-10 after:h-full after:w-32 after:bg-gradient-to-l dark:after:from-gray-900">
-          <div className="animate-slide flex w-[calc(125px*18)]">
+          <div className="animate-slide-reverse flex w-[calc(125px*18)]">
             {[...secondBlockLogos, ...secondBlockLogos].map((logo, index) => (
               <div
                 key={index}
