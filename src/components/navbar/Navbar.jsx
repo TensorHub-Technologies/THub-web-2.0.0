6eece911ba99d20ddd33f44f8423a4513a272922
ganesh <msganesh@digisprint.com>
2025-05-28 15:28:39 +0530

    feat:prod error code push

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,6 @@ import { toggleMode } from "../../store/modeSlice";
 const Navbar = () => {
   const [activePage, setActivePage] = useState("/");
   const [open, setOpen] = useState(false);
-
   // redux
   const dispatch = useDispatch();
   const isDarkMode = useSelector((state) => state.customization.isDarkMode);
@@ -65,45 +64,28 @@ const Navbar = () => {
       <div className="mx-auto flex items-center justify-between">
         <img src={ThubLogo} className="h-10 w-38" alt="THub Logo" />
         <div className="flex-wrap items-center justify-between gap-8 ml-16 hidden md:flex">
-          {navItems.map((item) =>
-            item.name === "Login" ? (
-              <a
-                key={item.path}
-                href={import.meta.env.VITE_APP_URL}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={`cursor-pointer ${
-                  activePage === item.path
-                    ? "text-primary dark:text-primary-dark"
-                    : "text-secondary hover:text-primary dark:text-secondary-dark dark:hover:text-primary-dark"
-                }`}
-              >
-                {item.name}
-              </a>
-            ) : (
-              <div
-                key={item.path}
-                className={`cursor-pointer ${
-                  activePage === item.path
-                    ? "text-primary dark:text-primary-dark"
-                    : "text-secondary hover:text-primary dark:text-secondary-dark dark:hover:text-primary-dark"
-                }`}
-                onClick={() => handleNavigation(item.path)}
-              >
-                {item.name}
-              </div>
-            ),
-          )}
+          {navItems.map((item) => (
+            <div
+              key={item.path}
+              className={`cursor-pointer ${
+                activePage === item.path
+                  ? "text-primary dark:text-primary-dark "
+                  : "text-black hover:text-primary dark:text-secondary-dark dark:hover:text-primary-dark"
+              }`}
+              onClick={() => handleNavigation(item.path)}
+            >
+              {item.name}
+            </div>
+          ))}
 
           <div>
-            <a
-              href={import.meta.env.VITE_APP_URL}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-block border border-primary text-primary px-4 py-2 rounded transition-all duration-300 hover:bg-primary hover:text-black hover:border-primary dark:border-primary-dark dark:text-primary-dark dark:hover:bg-primary-dark dark:hover:text-black"
+            <button
+              className="border border-primary text-primary px-4 py-2 ml-3 rounded transition-all duration-300 hover:bg-primary hover:text-black hover:border-primary
+              dark:border-primary-dark dark:text-primary-dark dark:hover:bg-primary-dark dark:hover:text-black"
+              onClick={() => handleNavigation("/auth/register")}
             >
               Get started
-            </a>
+            </button>
           </div>
           <div onClick={handleToggleClick} className="cursor-pointer">
             <img src={isDarkMode ? TogglMode1 : TogglMode2} alt="toggle_mode" />
@@ -136,14 +118,13 @@ const Navbar = () => {
         ))}
 
         <div>
-          <a
-            href={import.meta.env.VITE_APP_URL}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block border border-primary text-primary px-4 py-2 rounded transition-all duration-300 hover:bg-primary hover:text-black hover:border-primary dark:border-primary-dark dark:text-primary-dark dark:hover:bg-primary-dark dark:hover:text-black"
+          <button
+            className="border border-primary text-primary px-4 py-2 ml-3 rounded transition-all duration-300 hover:bg-primary hover:text-black hover:border-primary
+              dark:border-primary-dark dark:text-primary-dark dark:hover:bg-primary-dark dark:hover:text-black"
+            onClick={() => handleNavigation("/auth/register")}
           >
             Get started
-          </a>
+          </button>
         </div>
         <div
           onClick={handleToggleClick}
