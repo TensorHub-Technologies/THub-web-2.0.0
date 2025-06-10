33d9176a42ab5e545e928c43e6abfe5f3581b14a
asamuel-digi <83590864+asamuel-digi@users.noreply.github.com>
2025-06-10 14:49:25 +0530

    Update Features_Anime_One.jsx

--- src/components/home/Features_Anime_One.jsx
+++ src/components/home/Features_Anime_One.jsx
@@ -8,7 +8,21 @@ import { useSelector } from "react-redux";
 function Features_Anime_One() {
   const navigate = useNavigate();
   const handleClick = () => {
-    navigate("/auth/login");
+  const hostname = window.location.hostname;
+  let url;
+  console.log(hostname, "hostname");
+  switch (hostname) {
+    case "localhost":
+      url = import.meta.env.VITE_THUB_WEB_SERVER_LOCAL_URL;
+      break;
+    case "thub-web-demo-378678297066.europe-west1.run.app":
+      url = import.meta.env.VITE_THUB_WEB_SERVER_DEMO_URL;
+      break;
+    default:
+      url = import.meta.env.VITE_THUB_WEB_SERVER_PROD_URL;
+      break;
+  }
+    window.open(url, "_blank");
   };
 
   const isDarkMode = useSelector((state) => state.customization.isDarkMode);
