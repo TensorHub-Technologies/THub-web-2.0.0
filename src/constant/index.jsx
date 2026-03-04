d0f8681a4a07e440b4f78e82efedbd9ceb7d63b1
saikumar bidave <saikumar.bidave@digitalsprint.ai>
2026-03-04 11:51:21 +0530

    new logo added

--- src/constant/index.jsx
+++ src/constant/index.jsx
@@ -1,12 +1,10 @@
 import saiPic from "../assets/images/team/Saikumar1.png";
 import ganeshPic from "../assets/images/team/Ganesh-pic.jpg";
 import abhishekPic from "../assets/images/team/Abhisek_passport.png";
-import sristiPic from "../assets/images/team/Srishti_pic.png";
 import prasadPic from "../assets/images/team/P S V Prasad.png";
 import govardhanPic from "../assets/images/team/Govardhan.jpeg";
 import ceoPic from "../assets/images/team/GS_Oct.png";
 import aditya from "../assets/images/team/Aadithya2.png";
-import pp from "../assets/images/team/Pp.jpeg";
 import mahadev from "../assets/images/team/mahadev.png";
 
 export const teamMembers = [
@@ -33,48 +31,34 @@ export const teamMembers = [
   },
   {
     id: 4,
-    name: "Prathyanka S",
-    designation: "Founder & Business Analyst",
-    image: pp,
-    linkedIn: "https://www.linkedin.com/in/prathyanka-s-2897ba231/",
-  },
-  {
-    id: 5,
     name: "Abhishek Samuel",
     designation: "Cloud & AI Architect",
     image: abhishekPic,
     linkedIn: "https://www.linkedin.com/in/abhishek-yovan-b120a2214/",
   },
   {
-    id: 6,
-    name: "Srishti Mishra",
-    designation: "UI & AI Designer",
-    image: sristiPic,
-    linkedIn: "https://www.linkedin.com/in/srishti-mishra-149601145/",
-  },
-  {
-    id: 7,
+    id: 5,
     name: "Saikumar",
     designation: "Full Stack & Gen AI Developer",
     image: saiPic,
     linkedIn: "https://www.linkedin.com/in/saikumar-bidave-278882215/",
   },
   {
-    id: 8,
+    id: 6,
     name: "Ganesh M S",
     designation: "Full Stack & Gen AI Developer",
     image: ganeshPic,
     linkedIn: "https://www.linkedin.com/in/ganesh-savadattimath-350a32212/",
   },
   {
-    id: 9,
+    id: 7,
     name: "P S V Prasad",
     designation: "Data Scientist & Gen AI Developer",
     image: prasadPic,
     linkedIn: "https://www.linkedin.com/in/p-s-v-prasad-480b51243/",
   },
   {
-    id: 10,
+    id: 8,
     name: "Mahadev Bamma",
     designation: "Gen AI Developer",
     image: mahadev,
